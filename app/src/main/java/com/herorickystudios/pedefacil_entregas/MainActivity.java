88e77a7682ae73e2add5926333d0c1cd4c545072
HeroRickyGAMES <ricojn9@gmail.com>
2022-11-27 21:15:22 -0300

    Começo do projeto

---
+++ app/src/main/java/com/herorickystudios/pedefacil_entregas/MainActivity.java
@@ -0,0 +1,16 @@
+package com.herorickystudios.pedefacil_entregas;
+
+//Programado por HeroRickyGames
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
