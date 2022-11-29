b8ef95d08294fbca2b888df2a8e10d527f6fd8ac
HeroRickyGAMES <ricojn9@gmail.com>
2022-11-29 16:35:24 -0300

    Foi adicionado o serviço de notificações

---
+++ app/src/main/java/com/herorickystudios/pedefacil_entregas/NotifyVerify.java
@@ -0,0 +1,57 @@
+package com.herorickystudios.pedefacil_entregas;
+
+//Programado por HeroRickyGames
+
+import static android.Manifest.permission.POST_NOTIFICATIONS;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+
+public class NotifyVerify extends AppCompatActivity {
+    private View view;
+    private static final int PERMISSION_REQUEST_CODE = 200;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notify_verify);
+
+        verifyNotifyPermission(view);
+    }
+    public void verifyNotifyPermission(View view){
+
+        if (!checkPermission()) {
+
+            requestPermission();
+
+        } else {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        }
+    }
+
+
+    private boolean checkPermission() {
+        int result = ContextCompat.checkSelfPermission(getApplicationContext(), POST_NOTIFICATIONS);
+
+        return result == PackageManager.PERMISSION_GRANTED;
+
+    }
+
+    private void requestPermission() {
+
+        ActivityCompat.requestPermissions(this, new String[]{POST_NOTIFICATIONS}, PERMISSION_REQUEST_CODE);
+
+
+    }
+    public void Intent(View view){
+
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/herorickystudios/pedefacil_entregas/VeryfySDK.java
@@ -0,0 +1,29 @@
+package com.herorickystudios.pedefacil_entregas;
+
+//Programado por HeroRickyGames
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+
+public class VeryfySDK extends AppCompatActivity {
+
+    int AndroidVersionSDK = Build.VERSION.SDK_INT;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_veryfy_sdk);
+
+        if(AndroidVersionSDK < 33){
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+
+        }else{
+            Intent intent = new Intent(this, NotifyVerify.class);
+            startActivity(intent);
+        }
+    }
+}
