c57e4a75da860c169fd21add577ca80b5c1a1e01
HeroRickyGAMES <ricojn9@gmail.com>
2022-11-30 22:29:33 -0300

    Adiçao do Gerencianet para pagamentos

--- app/src/main/java/com/herorickystudios/pedefacil_entregas/MainActivity.java
+++ app/src/main/java/com/herorickystudios/pedefacil_entregas/MainActivity.java
@@ -7,21 +7,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.components.BuildConfig;
-import com.google.firebase.database.annotations.NotNull;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import org.json.JSONObject;
 
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import br.com.gerencianet.mobile.Gerencianet;
+import br.com.gerencianet.mobile.exceptions.GerencianetException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -79,6 +82,21 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+
+        HashMap<String, Object> options = new HashMap();
+
+        options.put("client_id", "client_id ");
+
+        options.put("client_secret", "client_secret");
+
+        options.put("sandbox", true);
+
+        try {
+            Gerencianet gn = new Gerencianet(options);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 }
