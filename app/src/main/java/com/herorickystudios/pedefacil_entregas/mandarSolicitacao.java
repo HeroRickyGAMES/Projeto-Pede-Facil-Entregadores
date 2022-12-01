852462895964f3fdcfba94d60d657c4f7684aef3
HeroRickyGAMES <ricojn9@gmail.com>
2022-12-01 11:22:43 -0300

    Adição de intens na interface, adição de um custom title na activity de solicitações

--- app/src/main/java/com/herorickystudios/pedefacil_entregas/mandarSolicitacao.java
+++ app/src/main/java/com/herorickystudios/pedefacil_entregas/mandarSolicitacao.java
@@ -1,8 +1,14 @@
 package com.herorickystudios.pedefacil_entregas;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.TypefaceSpan;
 
 public class mandarSolicitacao extends AppCompatActivity {
 
@@ -10,5 +16,7 @@ public class mandarSolicitacao extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mandar_solicitacao);
+
+        setTitle("Adicionar Item");
     }
 }
